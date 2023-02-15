8bb398a839ce1c94158d4a0df5396ca35f29fd4a
Tomasz Cebula <tcebula@pm.me>
2023-02-15 23:06:48 +0100

    Fix recorder logging, add extension skeleton

--- initdb/1_create_database_and_user.sh
+++ initdb/1_create_database_and_user.sh
@@ -6,6 +6,6 @@ pwd=$(<$RECORDER_PASSWORD_FILE)
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER recorder WITH LOGIN PASSWORD '$pwd';
     CREATE DATABASE salty;
-    ALTER DATABASE salty OWNER TO recorder;
     GRANT pg_write_all_data TO recorder;
+    GRANT pg_read_all_data TO recorder;
 EOSQL
